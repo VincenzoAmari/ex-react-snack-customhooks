ccb359180732d3a87fab06144d15432f59c67fb5
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-21 20:00:56 +0200

    snack 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import useSwitch from "./useSwitch";
+// snack1
+
+// // import useSwitch from "./useSwitch";
+
+// function App() {
+//   const [isOn, toggle] = useSwitch();
+
+//   return (
+//     <div>
+//       <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+//       <button onClick={toggle}>Cambia Stato</button>
+//     </div>
+//   );
+// }
+
+// snack2
+
+import useDate from "./useDate.jsx";
 
 function App() {
-  const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
 
   return (
     <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
+      <h1>Data e ora attuali:</h1>
+      <p>{currentDate.toLocaleString()}</p>
     </div>
   );
 }
